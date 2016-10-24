e2c185aff67b322f2774a40852598ed16b5b2cc7
Joachim Deelen <jdeelen@aquarius-software.de>
2016-10-25 01:05:24 +0200

    Initial import for Xcode base project files

---
+++ Apocha/Apocha/Apocha.h
@@ -0,0 +1,19 @@
+//
+//  Apocha.h
+//  Apocha
+//
+//  Created by Joachim Deelen on 25.03.16.
+//  Copyright © 2016 micabo software UG. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Apocha.
+FOUNDATION_EXPORT double ApochaVersionNumber;
+
+//! Project version string for Apocha.
+FOUNDATION_EXPORT const unsigned char ApochaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Apocha/PublicHeader.h>
+
+

c23464a7b16cf15dfe23b139f60ffa35f325c03d
Joachim Deelen <jdeelen@aquarius-software.de>
2016-11-02 00:41:22 +0100

    Further Development

--- Apocha/Apocha/Receipt.swift
+++ Apocha/Apocha/Receipt.swift
@@ -7,3 +7,27 @@
 //
 
 import Foundation
+
+public struct Receipt {
+	var bundleIdentifier: String
+	var appVersion: String
+	var opaqueValue: [UInt8]
+	var sha1Hash: [UInt8]
+	var originalApplicationVersion: String;
+	var receiptCreationDate: Date
+	var receiptExpirationDate: Date
+}
+
+public struct InAppPurchaseReceipt {
+	var quantity: Int
+	var productIdentifier: String
+	var transactionIdentifier: String
+	var originalTransactionIdentifier: String
+	var purchaseDate: Date
+	var originalPurchaseDate: Date
+	var subscriptionExpirationDate: Date
+	var cancellationDate: Date
+	var appItemId: String
+	var externalVersionIdentifier: String
+	var webOrderLineItemId: Int
+}

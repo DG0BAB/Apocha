c1d2b54767ca0781bf31bbb25ca1876b35021862
Joachim Deelen <jdeelen@micabo.de>
2016-11-07 00:37:07 +0100

    Initial Import

---
+++ Apocha/Apocha/Error.swift
@@ -0,0 +1,9 @@
+//
+//  Error.swift
+//  Apocha
+//
+//  Created by Joachim Deelen on 06.11.16.
+//  Copyright © 2016 micabo software UG. All rights reserved.
+//
+
+import Foundation
---
+++ Apocha/Apocha/ReceiptHandler.swift
@@ -0,0 +1,9 @@
+//
+//  ReceiptHandler.swift
+//  Apocha
+//
+//  Created by Joachim Deelen on 05.11.16.
+//  Copyright © 2016 micabo software UG. All rights reserved.
+//
+
+import Foundation
